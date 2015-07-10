8b8b7f92975889b4ce81c0bd75945eb9a2beb13f
Hiroyuki Kawagashira <kawagashira.hiroyuki@sharp.co.jp>
2015-07-11 01:04:11 +0900

    Update sendMessage method for several button click events. Add Javadoc style comments.

--- app/src/main/java/com/myappportfolio/MainActivity.java
+++ app/src/main/java/com/myappportfolio/MainActivity.java
@@ -38,11 +38,32 @@ public class MainActivity extends ActionBarActivity {
     return super.onOptionsItemSelected(item);
   }
 
+  /**
+   * This method is called when a button clicked.
+   *
+   * @attention This method must to be defined as public, void, and just having a abstruct, view.
+   * @todo (Hiroyuki Kawagashira) Improve click feeling.
+   */
   public void sendMessage(View view) {
     String str = "Unknown view was clicked";
     switch (view.getId()) {
       case R.id.button:
-        str = "This button will launch my spotify streamer";
+        str = "This button will launch my spotify streamer!";
+        break;
+      case R.id.button2:
+        str = "This button will launch my score app!";
+        break;
+      case R.id.button3:
+        str = "This button will launch my library app!";
+        break;
+      case R.id.button4:
+        str = "This button will launch my build it bigger!";
+        break;
+      case R.id.button5:
+        str = "This button will launch my xyz reader!";
+        break;
+      case R.id.button6:
+        str = "This button will launch my capstone app!";
         break;
       default:
         break;
